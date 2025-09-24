470c0d5b0eb2a79f266468f3bc631617ab8980de
Gayoma <Gayoma@GAYOMAYUSI>
2025-09-24 18:52:17 +0800

    initial commit for creating a threading.Current Thread

--- Basic_Threading/MyThreadClass.cs
+++ Basic_Threading/MyThreadClass.cs
@@ -11,7 +11,12 @@ namespace Basic_Threading
     {
         public static void Thread1()
         {
-
+            for (int loopCount = 0; loopCount < 6; loopCount++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopCount);
+                Thread.Sleep(1500);
+            }
         }
 
     }
