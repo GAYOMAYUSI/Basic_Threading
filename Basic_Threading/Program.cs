d13301787ff55c8b61e71dbe2f152fbee340e594
Gayoma <Gayoma@GAYOMAYUSI>
2025-09-24 18:54:27 +0800

    to create the application enableVisualStyles and renderingDefault

--- Basic_Threading/Program.cs
+++ Basic_Threading/Program.cs
@@ -14,7 +14,9 @@ namespace Basic_Threading
         [STAThread]
         static void Main()
         {
-         
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FrmBasicThread());
         }
     }
 }
